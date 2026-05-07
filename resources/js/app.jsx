8be991ca818eb23c081e32c4a266a05295b72908
laylaksmn <layla.laksmana26@gmail.com>
2026-05-07 19:34:39 +0700

    Field input dan output TranslatePage

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,10 +1,33 @@
 // import "./bootstrap";
-import React from "react";
+import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 
-const App = () => {
-    return <h1>React Berhasil Terpasang!</h1>;
-};
+function Translate() {
+    const [val, setVal] = useState("");
+    const [result, setResult] = useState("");
+    const change = (e) => {
+        setVal(e.target.value);
+    };
+    const click = () => {
+        setResult(val); //nnti konek ke API
+    };
+    return (
+        <div className="Fields">
+            <div className="InputField">
+                <input
+                    onChange={change}
+                    value={val}
+                    placeholder="Paste your function here..."
+                />
+                <button onClick={click}>Translate</button>
+            </div>
+
+            <div className="OutputField">
+                <input value={result} readOnly />
+            </div>
+        </div>
+    );
+}
 
 const root = createRoot(document.getElementById("app"));
-root.render(<App />);
+root.render(<Translate />);
