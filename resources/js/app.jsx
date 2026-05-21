b29d4465f76a9b5960c380c545856a7ab425006c
laylaksmn <layla.laksmana26@gmail.com>
2026-05-21 18:56:16 +0700

    Enhance translate UI

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -9,7 +9,7 @@ const languages = [
 function SelectLang({ value, onChange }) {
     return (
         <select
-            className="rounded-md border px-3 py-2"
+            className="rounded-full border border-gray-200 px-3 py-2"
             value={value}
             onChange={(e) => onChange(e.target.value)}
         >
@@ -48,77 +48,94 @@ function Translate() {
     };
 
     return (
-        <div className="max-w-4xl mx-auto p-6">
-            <div className="flex items-center gap-3">
-                <img src="images/logo.png" alt="Logo" className="w-8 h-8" />
-                <p className="font-bold text-3xl">SYNTAXTRANSLATE</p>
+        <div className="w-screen p-8">
+            <div className="w-full p-6 rounded-full bg-white border border-gray-200">
+                <div className="flex items-center gap-3">
+                    <img src="images/logo.png" alt="Logo" className="w-8 h-8" />
+                    <p className="font-bold text-3xl">SYNTAXTRANSLATE</p>
+                </div>
             </div>
 
-            <div></div>
-            <div className="w-xs p-12 flex flex-col gap-5">
-                <p className="font-bold text-2xl">WORKSPACE</p>
-                <nav className="flex items-center gap-3 rounded-full bg-blue-500 text-white px-4 py-2">
-                    <img
-                        src="images/translate-icon-active.png"
-                        alt="icon"
-                        className="w-5 h-5 m-2"
-                    />
-                    <a href="#" className="">
-                        TRANSLATE
-                    </a>
-                </nav>
-                <nav className="flex items-center gap-3 px-4 py-2">
-                    <img
-                        src="images/compare-icon.png"
-                        alt="icon"
-                        className="w-5 h-5 m-2"
-                    />
-                    <a href="#" className="hover:">
-                        COMPARE
-                    </a>
-                </nav>
-                <nav className="flex items-center gap-3 px-4 py-2">
-                    <img
-                        src="images/beautify-icon.png"
-                        alt="icon"
-                        className="w-5 h-5 m-2"
-                    />
-                    <a href="#" className="hover:">
-                        BEAUTIFY
-                    </a>
-                </nav>
-            </div>
-
-            <div className="flex justify-between items-center">
-                <p className="font-bold text-3xl">TRANSLATE_SYNTAX</p>
-                <button
-                    className="mt-4 rounded-full bg-blue-500 px-9 py-3 text-white hover:bg-blue-600"
-                    onClick={click}
-                >
-                    TRANSLATE
-                </button>
-            </div>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
-                <div className="flex flex-col gap-2">
-                    <p className="font-bold text-lg">SOURCE_CODE</p>
-                    <SelectLang value={sourceLang} onChange={setSourceLang} />
-                    <textarea
-                        className="min-w-100 min-h-150 rounded-xl border border-gray-200 p-5"
-                        value={sourceCode}
-                        onChange={(e) => setSourceCode(e.target.value)}
-                        placeholder="Paste function here..."
-                    />
+            <div className="grid grid-cols-1 md:grid-cols-[1fr_4fr] mt-8">
+                <div className="w-xs p-6 flex flex-col rounded-3xl bg-white border border-gray-200">
+                    <p className="font-bold text-2xl">WORKSPACE</p>
+                    <nav className="flex items-center gap-3 rounded-full bg-blue-500 text-white px-4 py-2 mt-6">
+                        <img
+                            src="images/translate-icon-active.png"
+                            alt="icon"
+                            className="w-5 h-5 m-2"
+                        />
+                        <a href="/" className="">
+                            TRANSLATE
+                        </a>
+                    </nav>
+                    <nav className="flex items-center hover:bg-[#F3F8FF] gap-3 px-4 py-2">
+                        <img
+                            src="images/compare-icon.png"
+                            alt="icon"
+                            className="w-5 h-5 m-2"
+                        />
+                        <a href="/compare" className="hover:">
+                            COMPARE
+                        </a>
+                    </nav>
+                    <nav className="flex items-center hover:bg-[#F3F8FF] gap-3 px-4 py-2">
+                        <img
+                            src="images/beautify-icon.png"
+                            alt="icon"
+                            className="w-5 h-5 m-2"
+                        />
+                        <a href="/beautify" className="hover:">
+                            BEAUTIFY
+                        </a>
+                    </nav>
                 </div>
 
-                <div className="flex flex-col gap-2">
-                    <p className="font-bold text-lg">TRANSLATED_CODE</p>
-                    <SelectLang value={targetLang} onChange={setTargetLang} />
-                    <textarea
-                        className="min-w-100 min-h-150 rounded-xl border border-gray-200 p-5"
-                        value={resultCode}
-                        readOnly
-                    />
+                <div className="">
+                    <div className="flex justify-between items-center mb-8">
+                        <p className="font-bold text-3xl">TRANSLATE_SYNTAX</p>
+                        <button
+                            className="rounded-full bg-blue-500 px-9 py-3 text-white hover:bg-blue-600"
+                            onClick={click}
+                        >
+                            TRANSLATE
+                        </button>
+                    </div>
+
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                        <div className="flex flex-col gap-2 rounded-3xl border border-gray-200">
+                            <div className="flex justify-between items-center p-6">
+                                <p className="font-bold text-xl">SOURCE_CODE</p>
+                                <SelectLang
+                                    value={sourceLang}
+                                    onChange={setSourceLang}
+                                />
+                            </div>
+                            <textarea
+                                className="font-semibold text-lg w-full min-h-150 bg-white rounded-b-3xl border-t-1 border-gray-200 p-5"
+                                value={sourceCode}
+                                onChange={(e) => setSourceCode(e.target.value)}
+                                placeholder="Paste function here..."
+                            />
+                        </div>
+
+                        <div className="flex flex-col gap-2 rounded-3xl border border-gray-200">
+                            <div className="flex justify-between items-center p-6">
+                                <p className="font-bold text-xl">
+                                    TRANSLATED_CODE
+                                </p>
+                                <SelectLang
+                                    value={targetLang}
+                                    onChange={setTargetLang}
+                                />
+                            </div>
+                            <textarea
+                                className="font-semibold text-lg w-full min-h-150 bg-white rounded-b-3xl border-t-1 border-gray-200 p-5"
+                                value={resultCode}
+                                readOnly
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/compare', function () {
+    return view('compare');
+});
+
+Route::get('/beautify', function () {
+    return view('beautify');
+});
